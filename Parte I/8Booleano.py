27cc106ce2f5d111c8e99b2982f489c7f1a22518
DavidVilchis63 <davidvilchis63@gmail.com>
2022-08-22 15:29:41 -0500

    Prueba 3

--- Parte I/8Booleano.py
+++ Parte I/8Booleano.py
@@ -7,9 +7,36 @@ print(type(verdadero))
 print(type(falso))"""
 
 #Operadores relacionales
-
+"""
 num1 = 1000
 num2 = 500
+num3 = 1000
+cadena = "Comparaciones de strings"
+cadena1 = "Comparacion de strings"
 
 print(num1 > num2)
 print(num2 < num1)
+print(num1 >= num3)
+print(num1 == num2) #Igualación comparar si son iguales
+print(num1 != num2) #Diferente de
+
+print(cadena == cadena1)
+"""
+#Operadores Logicos (And, or, not)
+"""
+print(10 > 2 and 9 > 100) 
+print(88 <= 45 or 60 > 110)
+print(not 90 != 90)
+"""
+
+#Metodos booleanos
+
+cadena = "Metodos booleanos para Strings"
+
+print(cadena.startswith("M")) #Verifica si empieza con la letra entre parentesis
+print(cadena.endswith("M")) #Verifica si empieza con la letra entre parentesis
+
+cadena1 = "TODO MAYUSCULA"
+
+print(cadena1.isupper())
+print(cadena1.islower())
---
+++ Parte I/9Condicionales.py
@@ -0,0 +1,14 @@
+# Condional if and else
+
+"""
+Edad = 17
+
+if Edad >= 18:
+    print("Eres mayor de edad")
+
+else: 
+    print("No eres mayor de edad")
+
+"""
+
+# Condicional Elif
