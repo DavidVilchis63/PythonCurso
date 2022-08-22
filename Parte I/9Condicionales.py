5f077b4f63a3a6944a39bda03ad95e8238bf4e50
DavidVilchis63 <davidvilchis63@gmail.com>
2022-08-22 15:43:02 -0500

    Prueba4

--- Parte I/9Condicionales.py
+++ Parte I/9Condicionales.py
@@ -12,3 +12,23 @@ else:
 """
 
 # Condicional Elif
+
+letra = "u"
+
+if letra.lower() == "a":
+    print("Esta vocal es A")
+
+elif letra.lower() == "e":
+    print("Esta vocal es E")
+
+elif letra.lower() == "i":
+    print("Esta vocal es I")
+
+elif letra.lower() == "o":
+    print("Esta vocal es O")
+
+elif letra.lower() == "u":
+    print("Esta vocal es U")
+
+else:
+    print("Error ingrese una vocal")
