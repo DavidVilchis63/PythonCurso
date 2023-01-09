1a9876f425da4ccd0f1431aec48b512d03f2bfd1
DavidVilchis <davidvilchis63@gmail.com>
2023-01-09 11:51:00 -0600

    Teoria estructuras de datos

---
+++ Parte I/10EstructurasDatos.py
@@ -0,0 +1,17 @@
+#Ejemplo de listas:
+#Los datos se pueden cambiar
+
+EjemploLista = ["pan", 5, 30, "cadena"]
+#Posicion         0    1   2      3
+
+#Ejemplo de tuplas:
+#Estos datos no se pueden cambiar
+
+EjemploTupla = ("pan", 5, 30, "cadena")
+#Posicion         0    1   2      3
+
+#Ejemplo de diccionario:
+
+EjemploDiccionario = {"Uno": 1, "Dos": 2, "Tres": 3}
+#Posicion              Clave:Valor  Clave:Valor Clave:Valor
+
