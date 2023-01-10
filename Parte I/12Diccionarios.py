d1ff3f0efc2eb507515fc8460d2671f30fae2f9a
DavidVilchis <davidvilchis63@gmail.com>
2023-01-10 12:14:54 -0600

    Agregando metodos de diccionario

--- Parte I/12Diccionarios.py
+++ Parte I/12Diccionarios.py
@@ -23,4 +23,32 @@ print(diccionario1)
 """
 
 #Metodos de diccionarios
+#Eliminar elementos del diccionario
+"""
+diccionario2 = {1: 2, 2: 3, 3: 4}
+print(diccionario2)
+diccionario2.pop(3)
+print(diccionario2)
 
+#diccionario2.clear()
+print(diccionario2) #Limpia todos los elementos que esten en el diccionario
+
+#Obtener un valor con el metodo get
+print(diccionario2.get(2)) #Trae el valor de la llave que se ingrese
+
+#Recibe un valor de llave y lo agrega
+diccionario2.setdefault(3, 5)
+print(diccionario2)
+
+#Combinar valores de dos diccionarios
+diccionario3 ={4: 5, 6: 7}
+
+diccionario2.update(diccionario3)
+print(diccionario2)
+
+#Copiar diccionario
+diccionario2.copy()
+print(diccionario2)
+"""
+
+#Ejercicio 1
