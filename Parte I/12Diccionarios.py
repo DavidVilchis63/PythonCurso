3f19835304e814e2b110ca7035d298d396332674
DavidVilchis <davidvilchis63@gmail.com>
2023-01-10 11:32:56 -0600

    Finalizando con conceptos de diccionarios

--- Parte I/12Diccionarios.py
+++ Parte I/12Diccionarios.py
@@ -1,5 +1,6 @@
 #Diccionarios
 #Las claves no se pueden repetir
+"""
 
 diccionario = {"Ususario": "Wcoto", "Contrasenia": 12345}
 print(diccionario)
@@ -7,3 +8,19 @@ print(type(diccionario))
 
 diccionario1 = {"Nombre" : "Walter", "Apellido" : "Coto", "Estatura": 1.80}
 print(diccionario1.keys())
+print(diccionario1.values())
+print(diccionario1["Estatura"]) #De esta forma manda a llamar una clave especifica dentro del diccionario
+
+#Agregar valores al diccionario
+
+diccionario1["Peso"] = "58 kg"
+print(diccionario1)
+
+#Modificar un valor existente
+
+diccionario1["Nombre"] = "OtroValor"
+print(diccionario1)
+"""
+
+#Metodos de diccionarios
+
