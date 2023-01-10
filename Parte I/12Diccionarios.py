387cdeb21e0d37a690fa49e550dd88a13fc35910
DavidVilchis <davidvilchis63@gmail.com>
2023-01-10 11:24:44 -0600

    Iniciando con conceptos de diccionarios

---
+++ Parte I/12Diccionarios.py
@@ -0,0 +1,9 @@
+#Diccionarios
+#Las claves no se pueden repetir
+
+diccionario = {"Ususario": "Wcoto", "Contrasenia": 12345}
+print(diccionario)
+print(type(diccionario))
+
+diccionario1 = {"Nombre" : "Walter", "Apellido" : "Coto", "Estatura": 1.80}
+print(diccionario1.keys())
