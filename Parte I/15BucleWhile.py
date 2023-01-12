8fe9e20058f39b4e24cb3e6955360d7ed875fb71
DavidVilchis <davidvilchis63@gmail.com>
2023-01-12 09:54:51 -0600

    Finalizando ejercicio 2 de while

--- Parte I/15BucleWhile.py
+++ Parte I/15BucleWhile.py
@@ -4,16 +4,17 @@
 
 while i <= 10:
     print("Ejemplo de iterador: {} Salto".format(i))
-    i += 1 """
+    i += 1 
+"""
 
 #Ejercicio1
-
+"""
 numero = int(input("Ingrese un numero para obtener su tabla de multiplicar:"))
 
 i = 1
 n = 1
 
-print("Tabla del: ", i)
+print("Tabla del: ", numero)
 
 while i <= 10:
         
@@ -21,3 +22,14 @@ while i <= 10:
     print(" {} x {} = {}".format(numero,n,multiplicacion))
     n = n + 1
     i = i + 1
+"""
+
+#Ejercicio2
+
+edad = int(input("Escriba su edad: "))
+i = edad
+
+while i > 0:
+    print("Ya cumpliste: ", edad)
+    edad = edad - 1
+    i = i - 1
