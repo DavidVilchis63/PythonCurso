be350784c6b48b2d2a4b356c01c9610088c6f7ec
DavidVilchis <davidvilchis63@gmail.com>
2023-01-16 13:27:07 -0600

    Empezando teorias de funciones

---
+++ Parte I/17Funciones.py
@@ -0,0 +1,26 @@
+#Funciones propias de Python
+"""
+num = "70"
+lista = [12,60,30,-4,50]
+
+print(type(num))
+print(type(int(num)))
+print(type(float(num)))
+
+print(len(lista))
+print(max(lista))
+print(min(lista))
+"""
+
+#Crear funciones propias
+
+def pruebaFuncion():
+    print("Hola mundo")
+
+#pruebaFuncion()
+
+def tabla7():
+    for i in range(11):
+        print("7 x {} = {} ".format(i, i*7))
+
+tabla7()
