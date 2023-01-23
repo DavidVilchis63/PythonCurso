8e122a81f8e3ec7313bbeff0e8fd3ff4d249f218
DavidVilchis <davidvilchis63@gmail.com>
2023-01-23 13:36:50 -0600

    Definiendo parametro y argumentos

---
+++ Parte I/19ParametrosArgumentos.py
@@ -0,0 +1,8 @@
+#Parametros y argumentos
+
+def suma(num1,num2): #num son los parametros
+    resultado = num1 + num2
+    return resultado
+
+print(suma(10,10)) #Aqui van los argumentos, el valor que tomara cada variable (Parametros)
+
