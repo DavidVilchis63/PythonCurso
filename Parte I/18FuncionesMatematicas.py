9009dfaf079d3ff9666c7090647a826cf48f5175
DavidVilchis <davidvilchis63@gmail.com>
2023-01-23 12:41:13 -0600

    Inciando con funciones matematicas

---
+++ Parte I/18FuncionesMatematicas.py
@@ -0,0 +1,18 @@
+#Funciones matematicas
+
+import math
+import random
+
+print(math.pow(10,3)) #Eleva numeros al valor asignado
+
+print(math.sqrt(64)) #Calcula raiz cuadrada
+print(math.isqrt(64)) #Resultado entero
+
+print(math.sin(90)) #Calculo de seno, etc
+print(math.cos(90))
+print(math.tan(90))
+
+print(math.factorial(5)) #Sacar factoriales
+
+print(random.randint(0,100)) #Toma un numero al azar del rango indicado
+
