d1534ad894fa9641b9312ef1e13b35d04dae7b41
DavidVilchis <davidvilchis63@gmail.com>
2023-01-23 11:28:29 -0600

    Resolviendo ejercico 1

--- Parte I/17Funciones.py
+++ Parte I/17Funciones.py
@@ -13,7 +13,7 @@ print(min(lista))
 """
 
 #Crear funciones propias
-
+""" 
 def pruebaFuncion():
     print("Hola mundo")
 
@@ -23,4 +23,34 @@ def tabla7():
     for i in range(11):
         print("7 x {} = {} ".format(i, i*7))
 
-tabla7()
+tabla7() """
+
+#Ejercicio 1
+
+listaIncial = []
+
+def agregarElementos():
+    nuevoValor = int(input("Ingrese un numero nuevo: "))
+    listaIncial.append(nuevoValor)
+
+i = 0
+
+while i < 10:
+    agregarElementos()
+    i = i + 1   
+
+print(listaIncial)
+
+listaPares = []
+listaImpares = []
+
+def separarNumeros():
+    for numero in listaIncial:
+        if numero % 2 == 0:
+            listaPares.append(numero)
+        else:
+            listaImpares.append(numero)
+
+separarNumeros()
+print("La lista de numeros pares es: ", listaPares)
+print("La lista de numeros impares es: ",listaImpares)
