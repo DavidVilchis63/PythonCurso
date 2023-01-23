064634c9c41a4ea8718a438b036da07e62ce7df8
DavidVilchis <davidvilchis63@gmail.com>
2023-01-23 13:11:26 -0600

    Realizando ejercicio 1

--- Parte I/18FuncionesMatematicas.py
+++ Parte I/18FuncionesMatematicas.py
@@ -1,5 +1,5 @@
-#Funciones matematicas
-
+#Funciones matematicas 
+"""
 import math
 import random
 
@@ -16,3 +16,23 @@ print(math.factorial(5)) #Sacar factoriales
 
 print(random.randint(0,100)) #Toma un numero al azar del rango indicado
 
+#Nota: Para usar el valor return en funciones es necesario usar
+#      un print para que el valor se vea
+"""
+
+#Ejercicio 1
+
+def pedirValores():
+    num1 = float(input("Ingrese numero 1: "))
+    num2 = float(input("Ingrese numero 2: "))
+
+    if num1 > num2:
+        return 1
+
+    elif num1 < num2:
+        return -1
+
+    else:
+        return 0
+
+print(pedirValores())
