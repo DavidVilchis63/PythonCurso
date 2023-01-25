06078771346a1dfe2f6bc82b227850bbef958b53
DavidVilchis <davidvilchis63@gmail.com>
2023-01-25 11:51:40 -0600

    Metodo especiales

---
+++ Parte II/POO/04MetodosEspeciales.py
@@ -0,0 +1,17 @@
+#Metodo
+
+class FabricaTelefonos(): #Clase
+    def __init__(self, marca): #Metodo construtor
+        self.marca = marca
+        print("El objeto {} ha sido creado".format(self.marca))
+
+    def __str__(self):
+        return "El objeto es {}".format(self.marca)        
+        
+    def __del__(self): #Al finalizar elimina todo
+        print("El objeto {} ha sido destruido".format(self.marca))
+
+telefono = FabricaTelefonos("Nokia")
+print(telefono.marca)
+print(telefono)
+        
