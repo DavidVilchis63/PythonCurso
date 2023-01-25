fbb095fa7788eabfccb29f0d78c4819606e82175
DavidVilchis <davidvilchis63@gmail.com>
2023-01-25 10:06:37 -0600

    Resolviendo ejercicio 1 y 2

--- Parte I/19ParametrosArgumentos.py
+++ Parte I/19ParametrosArgumentos.py
@@ -1,13 +1,14 @@
 #Parametros y argumentos
-
+"""
 def suma(num1,num2): #num son los parametros
     resultado = num1 + num2
     return resultado
 
 print(suma(10,10)) #Aqui van los argumentos, el valor que tomara cada variable (Parametros)
+"""
 
 #Variables globales
-
+"""
 def valores():
     global var1  #global sirve para hacer una variable global
     global var2
@@ -21,11 +22,35 @@ print(valores())
 
 resta = var1 - var2
 print(resta)
+"""
 
 #Valores indefinidos
-
+"""
 def argumento(*num): #Al usar * los elementos ingresados se almacenan en una tupla
     return type(num)
 
 print(argumento(10,9,0))
+"""
 
+#Ejercicio 1
+
+def areaRectangulo(base, altura):
+    total = base * altura
+    return total
+
+print(areaRectangulo(10,5))
+
+def areaCirculo (radio):
+    import math
+
+    total = math.pi*(math.pow(radio,2))
+    return total
+
+print(areaCirculo(10))
+
+#Ejercicio 2
+
+def muestra(*valores):
+    return len(valores)
+
+print(muestra(10,3,4,5))
