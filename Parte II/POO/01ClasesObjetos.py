ac115eddfec9da9b2ec3634db5b67be0647d6496
DavidVilchis <davidvilchis63@gmail.com>
2023-01-25 10:49:29 -0600

    Programacion orientada a objetos, clases y objetos

---
+++ Parte II/POO/01ClasesObjetos.py
@@ -0,0 +1,12 @@
+#Ejemplo de clase y objetos
+
+class EjemploClase():  #Clase
+    pass
+
+print(type(EjemploClase))
+
+tipo = EjemploClase() #Objeto
+tipo2 = EjemploClase()
+
+print(type(tipo))
+print(type(tipo2))
