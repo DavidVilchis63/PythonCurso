7b2a35dbb163ea970716f7f8acd751650018faae
DavidVilchis <davidvilchis63@gmail.com>
2023-01-25 09:48:36 -0600

    Variables globales

--- Parte I/19ParametrosArgumentos.py
+++ Parte I/19ParametrosArgumentos.py
@@ -6,3 +6,19 @@ def suma(num1,num2): #num son los parametros
 
 print(suma(10,10)) #Aqui van los argumentos, el valor que tomara cada variable (Parametros)
 
+#Variables globales
+
+def valores():
+    global var1  #global sirve para hacer una variable global
+    global var2
+
+    var1 = 110
+    var2 = 40
+    resultado = var1 + var2
+    return resultado
+
+print(valores())
+
+resta = var1 - var2
+print(resta)
+
