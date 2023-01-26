34c5b53dbc5dc1a75f5ae4bd91386b8e6a740e4f
DavidVilchis <davidvilchis63@gmail.com>
2023-01-26 11:15:44 -0600

    Trabajando con herencia, y palabra super para evitar errores

---
+++ Parte II/02 Pilares de POO/05EspecialidadesHerencia.py
@@ -0,0 +1,14 @@
+#Posibles errores al trabjar con herencia
+
+class Animales():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+class Perro(Animales):
+    def __init__(self, nombre, sonido):
+        super().__init__(nombre)
+        self.sonido = sonido
+
+perro = Perro("Manchas", "Ladrido")
+print(perro.nombre)
+print(perro.sonido)
